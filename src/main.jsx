661884770592c5bca80f0d4ac6dbdff17cf8d32b
Pulpoide <joacolivero@gmail.com>
2024-10-18 02:25:46 -0300

    Cambio en main.jsx import de style

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-<<<<<<< Updated upstream
-//import './index.css'
-=======
 import './App.css'
->>>>>>> Stashed changes
 import { BrowserRouter as Router } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
