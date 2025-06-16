ad6c4b6e97ef4ecac5b024550903cd836ded7d59
Pulpoide <joacolivero@gmail.com>
2025-06-16 18:14:40 -0300

    fix(faqs): update typography styles for better readability
    fix(faqs): improve FAQ content clarity and formatting

--- src/components/Faqs.jsx
+++ src/components/Faqs.jsx
@@ -5,7 +5,7 @@ import { ExpandMore } from '@mui/icons-material';
 const Faqs = () => {
     return (
         <Box sx={{ width: '100%', backgroundColor: 'white', color: '#089342', overflowX: 'hidden', p: 0 }}>
-            <Typography variant='h4' component='h2' sx={{ mb: 4, textAlign: 'center' }}>
+            <Typography variant='h4' component='h2' sx={{ mb: 4, textAlign: 'center', color:'black', fontFamily: "Bungee, sans-serif",}}>
                 Preguntas frecuentes
             </Typography>
             <div>
@@ -15,13 +15,13 @@ const Faqs = () => {
                         aria-controls="panel1-content"
                         id="panel1-header"
                     >
-                        <Typography variant="h6" sx={{ fontWeight: 'bold', color: '#45A048' }}>
+                        <Typography variant="h6" sx={{  color: '#45A048', fontFamily: "Bungee hairline, sans-serif", fontWeight: 'bold' }}>
                             ¿Cómo me registro para sacar mi turno?
                         </Typography>
                     </AccordionSummary>
                     <AccordionDetails>
-                        <Typography sx={{ color: '#636368' }}>
-                            <b>Para poder reservar un turno, debes iniciar sesion en tu cuenta.</b> Ingresa tu dirección de correo electrónico, a la cual se te enviará un email con un código. Abre tu correo electronico e ingresa el código proporcionado para iniciar sesión. Una vez logueado podrás proceder con la reserva del turno.
+                        <Typography sx={{ color: '#636368' , fontFamily: "Fjalla One, sans-serif",}}>
+                            <b>Para poder reservar un turno, debes registrarte e iniciar sesion en tu cuenta.</b> Ingresa tu dirección de correo electrónico, a la cual se te enviará un email con un código. Abre tu correo electronico e ingresa el código proporcionado para activar tu cuenta. Una vez dentro podrás proceder con la reserva.
                         </Typography>
                     </AccordionDetails>
                 </Accordion>
@@ -31,13 +31,13 @@ const Faqs = () => {
                         aria-controls="panel2-content"
                         id="panel2-header"
                     >
-                        <Typography variant="h6" sx={{ fontWeight: 'bold', color: '#45A048' }}>
-                            No he recibido el correo electrónico con el código para iniciar sesión. ¿Qué debo hacer?
+                        <Typography variant="h6" sx={{color: '#45A048', fontFamily: "Bungee hairline, sans-serif", fontWeight: 'bold'}}>
+                            No he recibido el correo electrónico con el código para registarme y activar mi cuenta. ¿Qué debo hacer?
                         </Typography>
                     </AccordionSummary>
                     <AccordionDetails>
-                        <Typography sx={{ color: '#636368' }}>
-                            Si no encuentras el email en tu bandeja de entrada, te recomendamos que revises la bandeja de spam o correo no desdeado. Por cualquier otro inconveniente puedes comunicarte con el área de soporte.
+                        <Typography sx={{ color: '#636368' , fontFamily: "Fjalla One, sans-serif",}}>
+                            Si no encuentras el email en tu bandeja de entrada, te recomendamos que revises la <b>bandeja de spam o correo no deseado</b>. Por cualquier otro inconveniente puedes comunicarte con el área de soporte.
                         </Typography>
                     </AccordionDetails>
                 </Accordion>
@@ -47,14 +47,14 @@ const Faqs = () => {
                         aria-controls="panel3-content"
                         id="panel3-header"
                     >
-                        <Typography variant="h6" sx={{ fontWeight: 'bold', color: '#45A048' }}>
+                        <Typography variant="h6" sx={{fontFamily: "Bungee hairline, sans-serif", fontWeight: 'bold', color: '#45A048' }}>
                             ¿Cómo reservar un turno?
                         </Typography>
                     </AccordionSummary>
                     <AccordionDetails>
-                        <Typography sx={{ color: '#636368' }}>
-                            Luego de iniciar sesión, deberas seleccionar el deporte que desees. Deporturnos te mostrará una lista con los turnos disponibles según los filtros que hayas aplicado. Verifica que los datos que seleccionaste estén correctos (Día, horario y deporte). Selecciona el botón de "Confirmar reserva" y ¡Listo!. 
-                            <b>A continuación te llegará un email de confirmación con los datos de la reserva.</b> 
+                        <Typography sx={{ color: '#636368', fontFamily: "Fjalla One, sans-serif", }}>
+                            Luego de iniciar sesión, deberas seleccionar el deporte y cancha que desees reservar. Deporturnos te mostrará una lista con los turnos disponibles según los filtros que hayas aplicado. Verifica que los datos que seleccionaste sean correctos, selecciona el botón de <b>"Confirmar reserva"</b> y ¡Listo!. A continuación te llegará un <b>email de confirmación con los datos de la reserva y un código QR</b> que debes mostrar para ingresar a la cancha.
+
                         </Typography>
                     </AccordionDetails>
                 </Accordion>
@@ -64,13 +64,13 @@ const Faqs = () => {
                         aria-controls="panel4-content"
                         id="panel4-header"
                     >
-                        <Typography variant="h6" sx={{ fontWeight: 'bold', color: '#45A048' }}>
+                        <Typography variant="h6" sx={{ fontFamily: "Bungee hairline, sans-serif", fontWeight: 'bold', color: '#45A048' }}>
                             ¿Cómo sé si mi reserva esta confirmada?
                         </Typography>
                     </AccordionSummary>
                     <AccordionDetails>
-                        <Typography sx={{ color: '#636368' }}>
-                            Deberías haber recibido un correo electrónico de confirmación de reserva. Te sugerimos que revises tu carpeta de spam o correo no deseado por si acaso. Si no encuentras el correo electrónico de confirmación, tambien puedes acceder a la sección "Mis reservas" de tu cuenta en la pagina de www.deporturrnos.com. En el caso de que el turno este confirmado lo verás reflejado en tus reservas activas. 
+                        <Typography sx={{ color: '#636368', fontFamily: "Fjalla One, sans-serif", }}>
+                            Deberías haber recibido un correo electrónico de confirmación de reserva. Te sugerimos que revises tu carpeta de spam o correo no deseado por si acaso. Si no encuentras el correo electrónico de confirmación, tambien puedes acceder a la sección <b>"Mis reservas"</b> de tu cuenta. En el caso de que el turno este confirmado lo verás reflejado en tus reservas activas. 
                         </Typography>
                     </AccordionDetails>
                 </Accordion>
