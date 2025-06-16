a9943c72d39530a59f7d38e36d0f333cd067aacf
Pulpoide <joacolivero@gmail.com>
2025-06-16 18:14:24 -0300

    fix(advertising): ensure buttons have consistent size across devices
    fix(advertising): adjust hover behavior for second button
    
    feat(advertising): center content vertically for better alignment

--- src/components/Advertising.jsx
+++ src/components/Advertising.jsx
@@ -27,8 +27,6 @@ const theme = createTheme({
 const StyledButton = styled(Button)(({ theme }) => ({
   backgroundColor: '#4CAF50',
   color: 'white',
-  padding: '10px 20px',
-  marginTop: '60px',
   borderRadius: '5px',
 }));
 
@@ -42,45 +40,57 @@ const Advertising = () => {
       backgroundImage: `url(${backgroundImage})`,
       backgroundSize: 'cover',
       backgroundPosition: 'center',
-      overflowX: 'hidden', 
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
       p: 0
     }}>
       <Container>
-        <Grid container spacing={4}>
-          <Grid item xs={12} sm={7}>
-            <Typography variant="h4" sx={{ fontWeight: 'bold', color: '#45A048', mt: '60px' }}>¿Te gustaría probar </Typography>
+        <Grid container spacing={4} alignItems="center" justifyContent="center" sx={{ height: '100%' }}>
+          <Grid item xs={12} sm={7} sx={{ textAlign: { xs: 'center', sm: 'left' }, mt: { xs: 2, sm: 0 } }}>
+            <Typography variant="h4" sx={{
+              fontFamily: "Bungee hairline, sans-serif",
+              fontWeight: 'bold', color: 'black', textAlign: 'center', fontSize: { xs: '2rem', md: '3rem' }
+            }}>¿Te gustaría probar </Typography>
             <Typography variant="h4" component="div"
               sx={{
                 flexGrow: 1,
-                fontFamily: "Bungee Shade, sans-serif",
-                fontWeight: "400",
+                fontFamily: "Bungee inline, sans-serif",
                 maxWidth: "fit-content",
-                cursor: "pointer",
-                flexShrink: 0,
-                color: 'black',
-                "&:hover": {
-                  color: theme.palette.primary.main 
-                },
-                mr: 'auto'
+                color: theme.palette.primary.main,
+                textAlign: 'center',
+                margin: '0 auto',
+                fontSize: { xs: '2rem', md: '3rem' }
               }}
               onClick={() => navigate("/")}
             >
               DEPORTURNOS
             </Typography>
-            <Typography variant="h4" sx={{ fontWeight: 'bold', color: '#45A048' }}>en tu complejo deportivo? </Typography>
+            <Typography variant="h4" sx={{fontFamily: "Bungee hairline, sans-serif", fontWeight: 'bold', color: 'black', textAlign: 'center', fontSize: { xs: '2rem', md: '3rem' } }}>en tu complejo deportivo? </Typography>
           </Grid>
 
-          <Grid item xs={12} sm={3}>
-            <StyledButton variant="contained" component="a" href="/planes">
+          <Grid item xs={12} sm={5} sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', gap: 0.3 }}>
+            <StyledButton variant="contained" component="a" href="/planes" sx={{fontFamily: "Bungee, sans-serif", fontSize: { xs: '0.9rem', sm: '1rem', md: '1rem' }, padding: { xs: '0.6rem 1.2rem', sm: '0.7rem 1.3rem', md: '0.8rem 1.5rem' }, minWidth: { xs: '230px', md: '260px' } }}>
               VER PLANES Y PRECIOS
             </StyledButton>
-            <StyledButton variant="contained"  component="a" href="/funcionalidades" sx={{backgroundColor:'white', color:'#45A048'}}>
+            <StyledButton variant="contained" component="a" href="/funcionalidades" sx={{
+              fontFamily: "Bungee, sans-serif",
+              backgroundColor: 'white',
+              color: '#45A048',
+              mt: 2,
+              fontSize: { xs: '0.9rem', sm: '1rem', md: '1rem' },
+              padding: { xs: '0.6rem 1.2rem', sm: '0.7rem 1.3rem', md: '0.8rem 1.5rem' },
+              minWidth: { xs: '230px', md: '260px' },
+              '&:hover': {
+                backgroundColor: 'white',
+                color: '#45A048',
+              }
+            }}>
               VER FUNCIONALIDADES
             </StyledButton>
           </Grid>
         </Grid>
       </Container>
-
     </Box>
   );
 };
