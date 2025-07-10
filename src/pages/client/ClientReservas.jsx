4c160aacee4619143c122ddaef2cad569a2b3052
Pulpoide <joacolivero@gmail.com>
2025-07-09 22:49:26 -0300

    style(client-reservas): improve overall styling

--- src/pages/client/ClientReservas.jsx
+++ src/pages/client/ClientReservas.jsx
@@ -1,8 +1,8 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import {
   Table, TableBody, TableCell, TableContainer, TableHead, TableRow,
   Paper, IconButton, Dialog, DialogActions, DialogContent,
-  DialogTitle, Button, Typography, Box
+  DialogTitle, Button, Typography, Box, useTheme, useMediaQuery, Stack
 } from '@mui/material';
 import DialogContentText from '@mui/material/DialogContentText';
 import CancelIcon from '@mui/icons-material/Cancel';
@@ -12,25 +12,43 @@ import { tableCellClasses } from '@mui/material/TableCell';
 import { useNavigate } from 'react-router-dom';
 import NavbarClient from "../../components/NavbarClient";
 import dayjs from 'dayjs';
+import { fontFamily, fontSize, margin, width } from "@mui/system";
 
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
   [`&.${tableCellClasses.head}`]: {
     backgroundColor: '#4CAF50',
     color: theme.palette.common.white,
+    fontFamily: 'Fjalla One, sans-serif',
+    fontSize: 16,
   },
   [`&.${tableCellClasses.body}`]: {
     fontSize: 14,
   },
 }));
-
-const tableButtonStyle = {
+const buttonStyle = {
   fontFamily: "Bungee Inline, sans-serif",
-  marginBottom: 2
+  marginBottom: 2,
+  width: '233px',
 };
+const cardStyle = {
+  fontFamily: 'Fjalla One, sans-serif',
+  fontSize: '16px',
+  marginBottom: '8px',
+  padding: '8px',
+  border: '1px solid #ccc',
+  borderRadius: '4px',
+  backgroundColor: '#f9f9f9',
+  textAlign: 'left',
+  boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)',
+  margin: '8px 0',
+};
+
+
 
 const ClientReservas = () => {
   const [reservas, setReservas] = useState([]);
   const [showCompleted, setShowCompleted] = useState(false);
+  const [showCancelled, setShowCancelled] = useState(false);
   const [open, setDialogOpen] = useState(false);
   const [reservaToCancelId, setReservaToCancelId] = useState(null);
   const navigate = useNavigate();
@@ -39,6 +57,8 @@ const ClientReservas = () => {
   const tokenConfig = {
     headers: { Authorization: `Bearer ${currentUser.token}` }
   };
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
 
   useEffect(() => {
     fetchReservas(showCompleted);
@@ -61,6 +81,10 @@ const ClientReservas = () => {
     setShowCompleted(prev => !prev);
   };
 
+  const handleShowCancelledToggle = () => {
+    setShowCancelled(prev => !prev);
+  };
+
   const handleCancelReservaClick = (id) => {
     setDialogOpen(true);
     setReservaToCancelId(id);
@@ -77,6 +101,11 @@ const ClientReservas = () => {
     fetchReservas(showCompleted);
   };
 
+  const filteredReservas = reservas.filter(reserva => {
+    if (!showCancelled && reserva.estado === 'CANCELADA') return false;
+    return true;
+  });
+
   return (
     <>
       <Box sx={{ backgroundColor: 'white', minHeight: '100vh' }}>
@@ -85,29 +114,45 @@ const ClientReservas = () => {
           <Typography variant="h4" fontFamily="Bungee Inline, sans-serif" sx={{ m: 3 }}>
             Mis Reservas
           </Typography>
-          <Button
-            variant="contained"
-            color={showCompleted ? "secondary" : "primary"}
-            onClick={handleShowCompletedToggle}
-            sx={tableButtonStyle}
+          <Stack
+            direction="column"
+            spacing={2}
+            alignItems="center"
+            sx={{ width: '100%' }}
           >
-            {showCompleted ? "Ocultar Completadas" : "Mostrar Completadas"}
-          </Button>
+            <Button
+              variant="contained"
+              color={showCompleted ? "black" : "primary"}
+              onClick={handleShowCompletedToggle}
+              sx={buttonStyle}
+            >
+              {showCompleted ? "Ocultar Completas" : "Mostrar Completas"}
+            </Button>
+            <Button
+              variant="contained"
+              color={showCancelled ? "black" : "primary"}
+              onClick={handleShowCancelledToggle}
+              sx={buttonStyle}
+            >
+              {showCancelled ? "Ocultar Canceladas" : "Mostrar Canceladas"}
+            </Button>
+          </Stack>
         </Box>
-        <TableContainer component={Paper}>
+
+        {!isMobile && (<TableContainer component={Paper} sx={{ display: { xs: 'none', sm: 'block' } }}>
           <Table>
             <TableHead>
               <TableRow>
-                <StyledTableCell align='right'>Fecha de creación</StyledTableCell>
-                <StyledTableCell align='right'>Fecha de turno</StyledTableCell>
-                <StyledTableCell align='right'>Hora de Inicio</StyledTableCell>
-                <StyledTableCell align='right'>Hora de Fin</StyledTableCell>
-                <StyledTableCell align='right'>Estado</StyledTableCell>
-                <StyledTableCell align='right'>Acciones</StyledTableCell>
+                <StyledTableCell align='center'>Fecha de Creación</StyledTableCell>
+                <StyledTableCell align='center'>Fecha de Turno</StyledTableCell>
+                <StyledTableCell align='center'>Hora de Inicio</StyledTableCell>
+                <StyledTableCell align='center'>Hora de Fin</StyledTableCell>
+                <StyledTableCell align='center'>Estado</StyledTableCell>
+                <StyledTableCell align='center'>Cancelar</StyledTableCell>
               </TableRow>
             </TableHead>
             <TableBody>
-              {reservas.length === 0 ? (
+              {filteredReservas.length === 0 ? (
                 <TableRow>
                   <TableCell colSpan={6} align="center">
                     <Typography variant="h6" color="textSecondary">
@@ -116,26 +161,28 @@ const ClientReservas = () => {
                   </TableCell>
                 </TableRow>
               ) : (
-                reservas.map((reserva) => (
+                filteredReservas.map((reserva) => (
+
                   <TableRow key={reserva.id}>
-                    <TableCell align='right'>{dayjs(reserva.fecha).format('DD/MM/YYYY')}</TableCell>
-                    <TableCell align='right'>{dayjs(reserva.turno.fecha).format('DD/MM/YYYY')}</TableCell>
-                    <TableCell align='right'>{reserva.turno.horaInicio}</TableCell>
-                    <TableCell align='right'>{reserva.turno.horaFin}</TableCell>
-                    <TableCell align='right'>
+                    <TableCell align='center'>{dayjs(reserva.fecha).format('DD/MM/YYYY')}</TableCell>
+                    <TableCell align='center'>{dayjs(reserva.turno.fecha).format('DD/MM/YYYY')}</TableCell>
+                    <TableCell align='center'>{reserva.turno.horaInicio}</TableCell>
+                    <TableCell align='center'>{reserva.turno.horaFin}</TableCell>
+                    <TableCell align='center'>
                       <Typography
                         style={{
                           color: reserva.estado === 'CONFIRMADA' ? 'green' :
                             reserva.estado === 'CANCELADA' ? 'red' :
-                              'default'
+                              'default',
+                          fontFamily: 'Bungee, sans-serif'
                         }}
                       >
                         {reserva.estado}
                       </Typography>
                     </TableCell>
-                    <TableCell align='right'>
+                    <TableCell align='center'>
                       {reserva.estado === 'CONFIRMADA' && (
-                        <IconButton color="secondary" onClick={() => handleCancelReservaClick(reserva.id)}>
+                        <IconButton color="warning" onClick={() => handleCancelReservaClick(reserva.id)}>
                           <CancelIcon />
                         </IconButton>
                       )}
@@ -151,24 +198,79 @@ const ClientReservas = () => {
             aria-labelledby="alert-dialog-title"
             aria-describedby="alert-dialog-description"
           >
-            <DialogTitle id="alert-dialog-title">
+            <DialogTitle id="alert-dialog-title" sx={{ fontFamily: 'Bungee, sans-serif', textAlign: 'center' }}>
               ¿Estás seguro de cancelar esta reserva?
             </DialogTitle>
             <DialogContent>
-              <DialogContentText id="alert-dialog-description">
+              <DialogContentText id="alert-dialog-description" sx={{ fontFamily: 'Fjalla One, sans-serif', textAlign: 'center' }}>
                 No podrás deshacer esta acción.
               </DialogContentText>
             </DialogContent>
             <DialogActions>
-              <Button onClick={handleAbortCancel} color="primary">
-                No cancelar
-              </Button>
-              <Button onClick={handleCancelReserva} color="error">
+              <Button onClick={handleCancelReserva} color="error" sx={{ fontFamily: 'Bungee, sans-serif' }}>
                 Cancelar reserva
+              </Button>
+              <Button onClick={handleAbortCancel} color="primary" sx={{ fontFamily: 'Bungee, sans-serif' }}>
+                No cancelar
               </Button>
             </DialogActions>
           </Dialog>
-        </TableContainer>
+        </TableContainer>)}
+
+
+        {isMobile && (<Box sx={{ display: { xs: 'block', sm: 'none' }, p: 2 }}>
+          {filteredReservas.map(reserva => (
+            <Paper key={reserva.id} sx={{ mb: 2, p: 2 }}>
+
+              <Typography variant="body2" sx={{
+                color: reserva.estado === 'CONFIRMADA' ? 'green' :
+                  reserva.estado === 'CANCELADA' ? 'red' : 'inherit',
+                fontFamily: 'Bungee, sans-serif', textAlign: 'center'
+              }}>
+                {reserva.estado}
+              </Typography>
+              <hr />
+
+              <Typography variant="body2" style={cardStyle}><strong>Fecha de creación:</strong> {dayjs(reserva.fecha).format('DD/MM/YYYY')}</Typography>
+              <Typography variant="body2" style={cardStyle}><strong>Turno:</strong> {dayjs(reserva.turno.fecha).format('DD/MM/YYYY')}</Typography>
+              <Typography variant="body2" style={cardStyle}><strong>Hora Inicio:</strong> {reserva.turno.horaInicio}</Typography>
+              <Typography variant="body2" style={cardStyle}><strong>Hora Fin:</strong> {reserva.turno.horaFin}</Typography>
+
+              {reserva.estado === 'CONFIRMADA' && (
+                <Box sx={{ textAlign: 'center' }}>
+                  <IconButton color="error" onClick={() => handleCancelReservaClick(reserva.id)}>
+                    <CancelIcon fontSize="large" />
+                  </IconButton>
+                </Box>
+              )}
+
+            </Paper>
+          ))}
+          <Dialog
+            open={open}
+            onClose={handleAbortCancel}
+            aria-labelledby="alert-dialog-title"
+            aria-describedby="alert-dialog-description"
+          >
+            <DialogTitle id="alert-dialog-title" sx={{ fontFamily: 'Bungee, sans-serif', textAlign: 'center' }}>
+              ¿Estás seguro de cancelar esta reserva?
+            </DialogTitle>
+            <DialogContent>
+              <DialogContentText id="alert-dialog-description" sx={{ fontFamily: 'Fjalla One, sans-serif', textAlign: 'center' }}>
+                No podrás deshacer esta acción.
+              </DialogContentText>
+            </DialogContent>
+            <DialogActions sx={{ justifyContent: 'center' }}>
+              <Button onClick={handleCancelReserva} color="error" sx={{ fontFamily: 'Bungee, sans-serif' }}>
+                Cancelar reserva
+              </Button>
+              <Button onClick={handleAbortCancel} color="primary" sx={{ fontFamily: 'Bungee, sans-serif' }}>
+                No cancelar
+              </Button>
+            </DialogActions>
+          </Dialog>
+        </Box>
+        )}
       </Box>
     </>
   );
